7a2f128c7cad61dc21cc9a50d8a8a485e9a91661
GianfrancoGrillo <89219968+GianfrancoGrillo@users.noreply.github.com>
2022-09-05 12:16:27 -0300

    header y footer

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Header from './components/header/Header'
+import Footer from './components/footer/Footer';
+import Home from './pages/Home/Home';
+import DetailMovies from './pages/DetailMovies/DetailMovies';
+import NotFound from './pages/NotFound/NotFound';
+import {Switch, Route} from 'react-router-dom'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Header/>
+
+    <Switch>
+          <Route path="/" exact >
+            <Home />
+          </Route>
+          {/* <Route path="/about" component={Characters} /> */}
+          <Route path="/movies/id/:id" component={DetailMovies} />
+          {/* <Route path="/contact" component={Contact}/> */}
+          <Route component={NotFound}/>
+    </Switch>
+
+    <Footer/>
+    </>
   );
 }
 
