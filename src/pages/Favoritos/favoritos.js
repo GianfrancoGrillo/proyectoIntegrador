6aefa3ac1909cbfa905bb38c721f3bfa8cac06af
Martiniano18 <martinianosanchez@udesa.edu.ar>
2022-09-17 19:18:15 -0300

    Finalizando botones

--- src/pages/Favoritos/favoritos.js
+++ src/pages/Favoritos/favoritos.js
@@ -11,10 +11,25 @@ class Favoritos extends Component {
   }
 
   componentDidMount() {
+    console.log('Primer renderizado')
     this.setState({ favoritos: JSON.parse(localStorage.getItem('favoritos')) })
   }
 
- 
+  
+ handleFavoritos(card){
+  console.log('Fede')
+  if (this.state.favoritos.some(fav => card.id === fav.id)) { 
+    console.log('Fede primer if')
+      this.setState({favoritos: this.state.favoritos.filter(item => item.id !== card.id)}, () => {//asincronismo del this.State ",()"
+          localStorage.setItem("favoritos", JSON.stringify(this.state.favoritos))
+      })
+  } else {                       
+      console.log('Fede segundo if')                                        
+      this.setState({favoritos: [...this.state.favoritos, card]}, () => { //el "..." significa traeme todo lo que estaba en el array favoritos y el ", card" significaa que le agrego la nueva card que te estoy pasando
+          localStorage.setItem("favoritos", JSON.stringify(this.state.favoritos))
+      })
+  }
+}
 
   render() {
     return (
--- src/pages/Home/home.js
+++ src/pages/Home/home.js
@@ -16,7 +16,6 @@ class Home extends Component {
   }    
 
  componentDidMount(){
-
     if (localStorage.length > 0) {
      this.setState({ favoritos: JSON.parse(localStorage.getItem('favoritos')) || [''] })
    } else {
@@ -47,21 +46,21 @@ class Home extends Component {
  }
 
  handleFavoritos(card){
-  if (this.state.favoritos.some(fav => card.id === fav.id)) {
+  console.log('Fede')
+  if (this.state.favoritos.some(fav => card.id === fav.id)) { 
+    console.log('Fede primer if')
       this.setState({favoritos: this.state.favoritos.filter(item => item.id !== card.id)}, () => {//asincronismo del this.State ",()"
           localStorage.setItem("favoritos", JSON.stringify(this.state.favoritos))
       })
-      console.log(this.state.favoritos.filter(item => item.id !== card.id))
-  } else {                                                               
+  } else {                       
+      console.log('Fede segundo if')                                        
       this.setState({favoritos: [...this.state.favoritos, card]}, () => { //el "..." significa traeme todo lo que estaba en el array favoritos y el ", card" significaa que le agrego la nueva card que te estoy pasando
           localStorage.setItem("favoritos", JSON.stringify(this.state.favoritos))
       })
   }
 }
  
-
 borrarTarjeta(id){
-
 const resto= this.state.populares.filter(populares=>  populares.id !== id)
 this.setState({
   populares: resto
